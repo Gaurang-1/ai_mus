96894bcbf6d34dfc869dc63a3430bb23cb0ff1f8
Gaurang-1 <72252176+Gaurang-1@users.noreply.github.com>
2021-04-02 12:24:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,78 @@
- var song1= "";
- var song2= "";
+song1 = "";
+song2 = "";
 
- function preload()
- {
-     song1 = loadSound("music.mp3");
-     song2 = loadSound("music2.mp3");
- }
- 
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
 
-function setup(){
-    canvas=createCanvas(500,400);
-    canvas.center();
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
 
-    video=createCapture(VIDEO);
-    video.hide();
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function preload()
+{
+	song1 = loadSound("music.mp3");
+	song2 = loadSound("music2.mp3");
 }
 
-function draw(){
-    image(video,0,0,500,400);
+function setup() {
+	canvas =  createCanvas(500, 400);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results);
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
+}
+
+function draw() {
+	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#FF0000");
+	stroke("#FF0000");
+
+	if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+        document.getElementById("display_song").innerHTML="Song Name - Harry porter theme"
+
+			song2.stop();
+
+		if(song1_status == false)
+		{
+			song1.play();
+			document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song"
+		}
+    }
 }
